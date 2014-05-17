5235eef82c25b2b94651a292b088b1a992609c20
xinyu198736 <xinyu198736@gmail.com>
2014-05-17 12:33:20 +0800

    将controller完全封装，约定大于配置

--- controllers/example.js
+++ controllers/example.js
@@ -2,43 +2,41 @@ var func_example;
 
 func_example = __F('example');
 
-module.exports.controllers = {
+module.exports = {
     "/": {
-        get: function(req, res, next) {
-            func_example.findAll().where({
-                id: {
-                    gt: 3
-                }
-            }).fields(['id', 'content']).raw(true).done(function(error, datas) {
-                if (error) {
-                    console.trace(error);
-                    next(error);
-                } else {
-                    res.send(datas);
-                }
-            });
+        get: function() {
+            this.useFilter(['example'])
+            return function(req, res, next) {
+                func_example.findAll().where({
+                    id: {
+                        gt: 3
+                    }
+                }).fields(['id', 'content']).raw(true).done(function(error, datas) {
+                    if (error) {
+                        next(error);
+                    } else {
+                        res.send(datas);
+                    }
+                });
+            }
+
         }
     },
     "/add": {
-        get: function(req, res, next) {
-            console.log(func_example.add)
-            func_example.add({
-                title: Math.random(),
-                content: Math.random()
-            }).done(function(error) {
-                if (error) {
-                    res.send(error.message);
-                } else {
-                    res.send("ok");
-                }
-            })
+        get: function() {
+            return function(req, res, next) {
+                func_example.add({
+                    title: Math.random(),
+                    content: Math.random()
+                }).done(function(error) {
+                    if (error) {
+                        res.send(error.message);
+                    } else {
+                        res.send("ok");
+                    }
+                });
+            }
 
         }
-    }
-};
-
-module.exports.filters = {
-    "/": {
-        get: ['example']
     }
 };
